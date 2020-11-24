d3db140d256ef7c8d88e6198304345e4be93fcd5
manoj <manoj.shrestha@aayulogic.com>
2020-11-25 00:22:44 +0545

    removed higher temperature test function

--- cypress/integration/pageobjects/weatherAverageCalcPage.js
+++ cypress/integration/pageobjects/weatherAverageCalcPage.js
@@ -16,17 +16,6 @@ class weatherAverageCalcPage {
             .click({force : true, timeout : 3000});
     }
 
-    calculateAverageOfHigherTemprature(){
-        var avg1 = 0;
-        cy.get('.DetailsSummary--highTempValue--3x6cL').each(($el, index, $list) => {
-            avg1 +=parseFloat($el.text())
-            if(index == $list.length - 1){
-                avg1 = avg1/$list.length;
-                cy.log("Average of Higher temprature leve = "+ avg1);
-            }
-        });
-    }
-
     calculateAverageOfLowerTemprature(){
         var avg = 0;
         cy.get('.DetailsSummary--lowTempValue--1DlJK').each(($el, index, $list) => {
